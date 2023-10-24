0775c531107d06c9f72b9bdde57d5fb9c4496a1e
Clara Gallardo <clara.gallardo93@gmail.com>
2023-10-24 18:57:25 +0200

    challenge2

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -32,3 +32,41 @@ ON titles.title_id = titleauthor.title_id
 INNER JOIN publishers
 ON publishers.pub_id = titles.pub_id
 GROUP BY publishers.pub_id, authors.au_id;
+
+
+---------- CHALLENGE 3 -------------
+
+SELECT authors.au_id AS 'AUTHOR ID',
+authors.au_lname AS 'LAST NAME',
+authors.au_fname AS 'FIRST NAME',
+SUM(sales.qty) AS 'TOTAL'
+FROM authors
+
+INNER JOIN titleauthor
+ON titleauthor.au_id = authors.au_id
+INNER JOIN titles
+ON titles.title_id = titleauthor.title_id
+INNER JOIN sales
+ON sales.title_id = titles.title_id
+
+GROUP BY authors.au_id
+ORDER BY TOTAL DESC
+LIMIT 3;
+
+-----------------CHALLENGE 4 ----------------
+
+SELECT 
+    authors.au_id AS 'AUTHOR ID',
+    CONCAT(authors.au_lname, ' ', authors.au_fname) AS 'FULLNAME',
+    IFNULL(SUM(sales.qty), 0) AS 'TOTAL'
+FROM
+    authors
+        LEFT JOIN
+    titleauthor ON titleauthor.au_id = authors.au_id
+        LEFT JOIN
+    titles ON titles.title_id = titleauthor.title_id
+        LEFT JOIN
+    sales ON sales.title_id = titles.title_id
+GROUP BY authors.au_id
+ORDER BY TOTAL DESC
+LIMIT 23;
