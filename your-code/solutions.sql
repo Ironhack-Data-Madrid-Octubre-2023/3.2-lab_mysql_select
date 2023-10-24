f1cfc361d9ffdacec858cffab1602406fc9d45e4
Clara Gallardo <clara.gallardo93@gmail.com>
2023-10-24 18:05:05 +0200

    challenge2

---
+++ your-code/solutions.sql
@@ -0,0 +1,34 @@
+-------------- CHALLENGE 1 -----------------
+
+SELECT authors.au_id AS 'AUTHOR ID',
+authors.au_lname AS 'LAST NAME',
+authors.au_fname AS 'FIRST NAME',
+titles.title AS 'TITLE',
+publishers.pub_name AS 'PUBLISHER'
+FROM authors
+
+INNER JOIN  titleauthor
+ON titleauthor.au_id = authors.au_id
+INNER JOIN titles
+ON titles.title_id = titleauthor.title_id
+INNER JOIN publishers
+ON publishers.pub_id = titles.pub_id;
+
+
+---------- CHALLENGE 2 -------------
+
+SELECT authors.au_id AS 'AUTHOR ID',
+authors.au_lname AS 'LAST NAME',
+authors.au_fname AS 'FIRST NAME',
+publishers.pub_name AS 'PUBLISHER',
+COUNT(titles.title) AS 'TITLE COUNT'
+FROM authors
+
+
+INNER JOIN titleauthor
+ON titleauthor.au_id = authors.au_id
+INNER JOIN titles
+ON titles.title_id = titleauthor.title_id
+INNER JOIN publishers
+ON publishers.pub_id = titles.pub_id
+GROUP BY publishers.pub_id, authors.au_id;
