847148c3bc873a16a5b1a9b75cd10527b2ecb2ea
Noelia Roson <noelia.roson@gmail.com>
2023-10-24 18:19:55 +0200

    lab terminado

---
+++ your-code/solutions.sql
@@ -0,0 +1,56 @@
+-- Challenge 1 - Who Have Published What At Where?
+
+USE publications;
+SELECT authors.au_id AS `AUTHOR ID`, 
+authors.au_lname AS `LAST NAME`, 
+authors.au_fname AS `FIRST NAME`, 
+titles.title AS `TITLE`,
+publishers.pub_name AS `PUBLISHER`
+FROM titleauthor
+LEFT JOIN authors ON titleauthor.au_id = authors.au_id
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+LEFT JOIN publishers ON titles.pub_id = publishers.pub_id;
+
+
+-- Challenge 2 - Who Have Published How Many At Where?
+
+USE publications;
+SELECT authors.au_id AS `AUTHOR ID`, 
+authors.au_lname AS `LAST NAME`, 
+authors.au_fname AS `FIRST NAME`, 
+publishers.pub_name AS `PUBLISHER`,
+COUNT(titles.title_id) AS `TITLE COUNT`
+FROM titleauthor
+LEFT JOIN authors ON titleauthor.au_id = authors.au_id
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+LEFT JOIN publishers ON titles.pub_id = publishers.pub_id
+GROUP BY authors.au_id, authors.au_lname, authors.au_fname, publishers.pub_name;
+
+-- Challenge 3 - Best Selling Authors
+
+USE publications;
+SELECT authors.au_id AS `AUTHOR ID`, 
+authors.au_lname AS `LAST NAME`, 
+authors.au_fname AS `FIRST NAME`, 
+SUM(sales.qty) AS `TOTAL`
+FROM titleauthor
+LEFT JOIN authors ON titleauthor.au_id = authors.au_id
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+LEFT JOIN sales ON titles.title_id = sales.title_id
+GROUP BY authors.au_id, authors.au_lname, authors.au_fname
+ORDER BY `TOTAL` DESC
+LIMIT 3;
+
+
+-- Challenge 4 - Best Selling Authors Ranking
+
+SELECT authors.au_id as 'AUTHOR ID',
+		authors.au_lname as 'LAST NAME',
+        authors.au_fname as 'FIRST NAME',
+        COALESCE(SUM(sales.qty), 0) as 'TOTAL'
+FROM authors 
+LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
+LEFT JOIN titles ON titleauthor.title_id = titles.title_id
+LEFT JOIN sales ON titles.title_id = sales.title_id
+GROUP BY authors.au_id, authors.au_lname, authors.au_fname
+ORDER BY `TOTAL` DESC;
