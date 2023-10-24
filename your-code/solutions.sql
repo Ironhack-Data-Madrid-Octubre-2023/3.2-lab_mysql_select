66041899d299ad856013e27c658db643b4cf5f9a
Borja Silla <borjasilla@gmail.com>
2023-10-24 15:25:05 +0200

    Fixed challenge 3 & 4

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -33,34 +33,33 @@ GROUP BY tit.title
 
 --- CHALLENGE 3 ---
 
-SELECT
-    titau.au_id AS 'Author ID',
-    pubau.au_lname AS 'Last Name',
-    pubau.au_fname AS 'First Name',
-    IFNULL(SUM(tit.ytd_sales),0) AS 'Total Titles Sold'
-FROM
-    publications.authors AS pubau
-LEFT JOIN
-    titleauthor AS titau ON pubau.au_id = titau.au_id
-INNER JOIN
-    titles AS tit ON titau.title_id = tit.title_id
-LEFT JOIN
-    sales AS sls ON tit.title_id = sls.title_id
-    
-GROUP BY pubau.au_id, pubau.au_lname, pubau.au_fname
-
-ORDER BY `Total Titles Sold` DESC
-
-LIMIT 3;
-
---- CHALLENGE 4 ---
-
-SELECT au.au_id, au.au_fname, au.au_lname, IFNULL(SUM(tit.ytd_sales),0) AS 'TOTAL SALES'
+SELECT au.au_id AS 'AUTHOR ID',
+		au.au_lname AS 'LAST NAME',
+		au.au_fname AS 'FIRST NAME',
+		IFNULL(SUM(tit.ytd_sales),0) AS 'Total Sales'
 
 FROM publications.authors as au
 LEFT JOIN titleauthor as titau
 ON au.au_id = titau.au_id
 LEFT JOIN titles as tit
+ON titau.title_id = tit.title_id
+
+GROUP BY `AUTHOR ID`
+ORDER BY `Total Sales` DESC
+LIMIT 3;
+
+--- CHALLENGE 4 ---
+
+SELECT au.au_id AS 'AUTHOR ID',
+		au.au_lname AS 'LAST NAME',
+		au.au_fname AS 'FIRST NAME',
+		IFNULL(SUM(tit.ytd_sales),0) AS 'Total Sales'
+
+FROM publications.authors as au
+LEFT JOIN titleauthor as titau
 ON au.au_id = titau.au_id
-GROUP BY au.au_id
-ORDER BY `TOTAL SALES` DESC;
+LEFT JOIN titles as tit
+ON titau.title_id = tit.title_id
+
+GROUP BY `AUTHOR ID`
+ORDER BY `Total Sales` DESC;
