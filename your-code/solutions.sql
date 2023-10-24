2b41d7f941ae97adfaaaada82729e13a2689eb11
Kohkitos <sanzyagain@gmail.com>
2023-10-24 16:09:49 +0200

    Finished

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -1,3 +1,4 @@
+-- CHALLENGE 1--
 SELECT 
     au.au_id AS 'AUTHOR ID',
     au_lname AS 'LAST NAME',
@@ -12,3 +13,56 @@ FROM
     titles AS tl ON tlau.title_id = tl.title_id
         LEFT JOIN
     publishers AS pb ON tl.pub_id = pb.pub_id;
+	
+-- CHALLENGE 2--
+SELECT 
+    au.au_id AS 'AUTHOR ID',
+    au_lname AS 'LAST NAME',
+    au_fname AS 'FIRST NAME',
+	pb.pub_name AS 'PUBLISHER', -- #4
+    COUNT(tlau.title_id) AS 'TITLE COUNT'
+FROM
+    authors AS au
+        INNER JOIN
+    titleauthor AS tlau ON au.au_id = tlau.au_id
+        INNER JOIN
+    titles AS tl ON tlau.title_id = tl.title_id
+        INNER JOIN
+    publishers AS pb ON tl.pub_id = pb.pub_id
+GROUP BY pb.pub_id, au.au_id
+ORDER BY `TITLE COUNT` DESC;
+
+--CHALLENGE 3--
+SELECT 
+    au.au_id AS 'AUTHOR ID',
+    au_lname AS 'LAST NAME',
+    au_fname AS 'FIRST NAME',
+    COUNT(sl.qty) AS 'TOTAL'
+FROM
+    authors AS au
+        INNER JOIN
+    titleauthor AS tlau ON au.au_id = tlau.au_id
+        INNER JOIN
+    titles AS tl ON tlau.title_id = tl.title_id
+        INNER JOIN
+    sales AS sl ON tl.title_id = sl.title_id
+GROUP BY au.au_id
+ORDER BY `TOTAL` DESC
+LIMIT 3;
+
+--CHALLENGE 4--
+SELECT 
+    au.au_id AS 'AUTHOR ID',
+    au_lname AS 'LAST NAME',
+    au_fname AS 'FIRST NAME',
+    COUNT(sl.qty) AS 'TOTAL'
+FROM
+    authors AS au
+        LEFT JOIN
+    titleauthor AS tlau ON au.au_id = tlau.au_id
+        LEFT JOIN
+    titles AS tl ON tlau.title_id = tl.title_id
+        LEFT JOIN
+    sales AS sl ON tl.title_id = sl.title_id
+GROUP BY au.au_id
+ORDER BY `TOTAL` DESC;
