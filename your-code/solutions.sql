e112e073fa5c8708129b343a33c78fb4812dbf54
Karmele Alonso Aia <karmele.alonso.aia@gmail.com>
2023-10-25 09:14:57 +0200

    algunos cambios

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -40,13 +40,13 @@ FROM titleauthor
   LIMIT 3;
   
 -- Challenge 4. Best selling authors ranking
-	USE publications;
+USE publications;
 
-	SELECT authors.au_id as `AUTHOR ID`,
-		authors.au_lname as `LAST NAME`,
-        authors.au_fname as `FIRST NAME`,
-        COALESCE(SUM(sales.qty), 0) as `TOTAL`
-	FROM authors
+SELECT authors.au_id as `AUTHOR ID`,
+authors.au_lname as `LAST NAME`,
+authors.au_fname as `FIRST NAME`,
+COALESCE(SUM(sales.qty), 0) as `TOTAL`
+FROM authors
 	LEFT JOIN titleauthor ON authors.au_id = titleauthor.au_id
 	LEFT JOIN titles ON titleauthor.title_id = titles.title_id
 	LEFT JOIN sales ON titles.title_id = sales.title_id
