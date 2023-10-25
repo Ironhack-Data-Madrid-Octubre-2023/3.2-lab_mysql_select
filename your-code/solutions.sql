2164ced5ba7e66455989d87a4eaaf7911352c43f
Blanca Abreu <blanca.abreu.guti@gmail.com>
2023-10-25 15:33:23 +0200

    siguiendo

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -1,3 +1,5 @@
+##Challenge 1 - Who Have Published What At Where?
+
 # Mirar Titles
 SELECT *
 FROM titles;
@@ -16,15 +18,17 @@ FROM titleauthor;
 # Queremos 3 columnas de authors, authors.au_id, authors.au_lname, authors.au_fname
 # Enlazarlo con, titles.title y publishers.pub_name
 
-
-SELECT authors.au_id, authors.au_fname, authors.au_lname, titles.title as TITLE, publishers.pub_name as PUBLISHER
+SELECT authors.au_id as `AUTHOR ID`, authors.au_fname as `FIRST NAME`, authors.au_lname as `LAST NAME`, titles.title as `TITLE`, publishers.pub_name as `PUBLISHER`
 FROM (authors
 INNER JOIN titleauthor ON titleauthor.au_id = authors.au_id
 INNER JOIN titles ON titles.title_id = titleauthor.title_id
 INNER JOIN publishers ON publishers.pub_id = titles.pub_id
 );
 
-#ME FALTA PONER EL NOMBRE BONITO DE LAS 3 PRIMERAS COLUNAS CON AS
+## Challenge 2 - Who Have Published How Many At Where?
 
 
+## Challenge 3 - Best Selling Authors
 
+
+## Challenge 4 - Best Selling Authors Ranking
